6644d9a66b8c7bbdce3d5c569cf1ccfc80f7e1c2
Rohit <stylishrb711@gmail.com>
2022-08-22 20:24:45 +0530

    models created!

---
+++ server/models/userSchema.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+// create schema
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  password2: {
+    type: String,
+    required: true,
+  },
+});
+
+// create collection
+
+// export
