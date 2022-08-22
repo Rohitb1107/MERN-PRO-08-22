d8ab150857d5ac0345455b8a18f5c5698e9f2284
Rohit <stylishrb711@gmail.com>
2022-08-22 16:19:04 +0530

    mongodb connected!

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server start running on port ${PORT}.`);
-});
+mongoose
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server starts running on port ${PORT}.`);
+    });
+  })
+  .catch((err) => {
+    console.log("error:", err);
+  });
